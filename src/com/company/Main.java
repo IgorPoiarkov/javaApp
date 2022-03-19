23e55ae692ae77671bbba9a8709a072b7043aac0
IgorPoiarkov <ip9169999968@yandex.ru>
2022-03-19 17:36:56 +0400

    first part

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.lang.String;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte num = 23; // Может принимать значения от -128 до 128
+        int num_1 = 123134;
+
+        float num_2 = 23.45f; // В конце поставить f;
+        double num_3 = 234.345763467467d;// Используется для очень больших чисел. В конце обязательно нужно поставить d;
+
+        boolean isTrue = true;
+        char sym = 'R'; //Используется для записи только одного символа!
+
+        String str = "Hello World!";
+
+        System.out.print(str);
+
+    }
+}
