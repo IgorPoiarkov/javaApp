0abba14818172ec020df8f9843fa0dd2535187fa
IgorPoiarkov <ip9169999968@yandex.ru>
2022-03-19 18:16:01 +0400

    second part

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,13 @@
 package com.company;
-import java.lang.String;
 import java.util.Scanner; // Класс позволяет получать информацию от пользователя
 
 
 public class Main {
 
     public static void main(String[] args) {
-        byte num = 23; // Может принимать значения от -128 до 128
-        int num_1 = 123134;
-
-        float num_2 = 23.45f; // В конце поставить f;
-        double num_3 = 234.345763467467d;// Используется для очень больших чисел. В конце обязательно нужно поставить d;
-
-        boolean isTrue = true;
-        char sym = 'R'; //Используется для записи только одного символа!
-
-        String str = "Hello World!";
-
-        System.out.print(str);
 
         Scanner vtr = new Scanner(System.in);
-        System.out.println(vtr.nextLine());
+        System.out.println("Your string is: " + vtr.nextLine());
 
     }
 }
